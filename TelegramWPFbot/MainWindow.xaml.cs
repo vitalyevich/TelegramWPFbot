2dab94a3e274438215c13ef832524d770d7fc8e1
vitalyevich <maksim_2411@mail.ru>
2021-02-08 16:23:31 +0300

    Добавьте файлы проекта.

---
+++ TelegramWPFbot/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using Telegram.Bot;
+
+namespace OPIS
+{
+    public partial class MainWindow : Window
+    {
+        TelegramBotClient bot = new TelegramBotClient(System.IO.File.ReadAllText("token.txt"));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bot.OnMessage += Bot_OnMessage;
+
+            bot.StartReceiving();
+
+        }
+
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var user = new Msg()
+                {
+                    firstName = e.Message.Chat.FirstName,
+                    msg = e.Message.Text,
+                    id = e.Message.Chat.Id,
+                    msgtype = e.Message.Type
+                };
+                listMessage.Items.Add(user);
+
+                bot.SendTextMessageAsync(user.id, $"Тип вашего сообщения {user.msgtype}");
+
+                if (user.msg == "Компьютер")
+                {
+                    bot.SendTextMessageAsync(user.id, "Компью́тер — устройство или система, способная выполнять заданную, чётко определённую, изменяемую последовательность операций. ");
+                }
+            });
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var user = (Msg)listMessage.SelectedItem;
+
+            bot.SendTextMessageAsync(user.id, txtMsg.Text);
+        }
+    }
+}
---
+++ TelegramWPFbot/Msg.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot.Types.Enums;
+namespace OPIS
+{
+    class Msg
+    {
+        public long id;
+        public string msg;
+        public string firstName;
+        public Telegram.Bot.Types.Enums.MessageType msgtype;
+        public override string ToString()
+        {
+            return $"{firstName}: {msg}";
+        }
+    }
+}
